33ed1c53a6218462d060511a9637c4a0ebdeb080
Cemonix <gittlermilan@seznam.cz>
2026-03-17 20:54:31 +0100

    Add new panel and asset manager into editor

--- hydratus-editor/src/editor.rs
+++ hydratus-editor/src/editor.rs
@@ -1,13 +1,17 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use hydratus_core::map::Map;
+use crate::asset_manager::{AssetManager, FileUri};
 use crate::panels::top_panel::top_panel;
 use crate::panels::left_panel::left_panel;
 use crate::panels::central_panel::central_panel;
+use crate::panels::bottom_panel::bottom_panel;
 
 pub struct MapEditor {
     map: Map,
-    background: Option<PathBuf>
+    asset_manager: AssetManager,
+    background: Option<FileUri>,
+    selected_tile: Option<String>
 }
 
 impl MapEditor {
@@ -16,15 +20,18 @@ impl MapEditor {
 
         MapEditor {
             map: Map::new(640, 480, 32),
-            background: None
+            asset_manager: AssetManager::load_assets(Path::new("hydratus-editor/assets")),
+            background: None,
+            selected_tile: None
         }
     }
 }
 
 impl eframe::App for MapEditor {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        top_panel(ctx, frame, &mut self.background);
-        left_panel(ctx, frame);
-        central_panel(ctx, frame, &mut self.background);
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        top_panel(ctx, &mut self.background);
+        left_panel(ctx);
+        central_panel(ctx, &mut self.background, &mut self.selected_tile);
+        bottom_panel(ctx, &self.asset_manager, &mut self.selected_tile);
     }
 }
