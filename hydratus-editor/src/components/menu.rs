69f5677586504fc592cbd30b48d1dc6c8d496215
Cemonix <gittlermilan@seznam.cz>
2026-03-17 20:54:00 +0100

    Update menu file handling

--- hydratus-editor/src/components/menu.rs
+++ hydratus-editor/src/components/menu.rs
@@ -1,17 +1,29 @@
-use std::path::PathBuf;
-
 use rfd::FileDialog;
 
-pub fn menu_bar(ui: &mut egui::Ui, background: &mut Option<PathBuf>) {
+use crate::asset_manager::FileUri;
+
+pub fn menu_bar(ui: &mut egui::Ui, background: &mut Option<FileUri>) {
     egui::MenuBar::new().ui(ui, |ui| {
         ui.menu_button("File", |ui| {
             if ui.button("Open file").clicked() {
-                *background = FileDialog::new()
+                let file_path = FileDialog::new()
                     .add_filter("image", &["jpeg", "jpg", "png", "bmp"])
                     .set_directory("~")
                     .pick_file();
 
-                
+                if let Some(path) = file_path {
+                    *background = match FileUri::try_from(path) {
+                        Ok(path) => Some(path),
+                        Err(_) => {
+                            rfd::MessageDialog::new()
+                                .set_level(rfd::MessageLevel::Error)
+                                .set_description(String::from("Could not open the file"))
+                                .set_title(String::from("Opening file error"))
+                                .show();
+                            None
+                        }
+                    };
+                }
             }
 
             if ui.button("Quit").clicked() {
